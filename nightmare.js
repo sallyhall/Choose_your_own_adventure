4fe947c856617b32ec059e7a97d8f80778521393
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-15 10:16:12 -0400

    added functonality to play game again

--- nightmare.js
+++ nightmare.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
-    console.log("ready!");
-    var choice;
+    //stores the amount to increase or decrease the score
     var increaseScore;
+    //selects the div that contains displayed text
     var promptText = $("#prompt");
+    //selects the div that contains displayed score
     var scoreText = $("#score");
+    //object to hold player information
     var knight = {
         weapon: "none",
-        setting: "home",
         score: 0,
-        };
-
+    };
+    //object to hold all the text for the decicion points
+    var decisions = {
+        welcome: "Welcome! Click on Steve to begin.",
+        begin: "Good morning! It's time for an adventure. Choose your weapon?",
+        setting: "Where does your adventure take you today?",
+        forest: "You've just stumbled on a creepy, dark cave. Do you throw a rock in first, or just march right in?",
+        defeatDragon: "You have awoken the dragon and he runs out of the cave right at you! Thank goodness you brought your sword. You have slain the dragon, stolen its treasure, and return to your castle as a hero. Huzzah!",
+        dragonDeath: "You have awoken the dragon and he runs out of the cave right at you! Your weapon is no match for this beast. You should have chosen your sword. The dragon destroys you in a single bite and your entire family forgets about you by Christmas.",
+        cave: "You have awoken the dragon and he runs out of the cave right at you! Your weapon is no match for this beast. You should have chosen your sword. The dragon destroys you in a single bite and your entire family forgets about you by Christmas.",
+        defeatSeaMonster: "You have encountered a sea monster! Thank goodness you brought your crossbow. You have slayed the sea monster and return to your castle a hero.",
+        seaMonsterDeath: "You have encountered a sea monster! Your weapon is no match for this beast. You should have chosen your crossbow. The sea monster destroys you in a single bite and your entire family forgets about you by Christmas.",
+        boat: "There is an empty boat! What do you do: Continue to walk up the beach, or get in the boat and paddle out to sea?",
+        mermaid: "You have encountered a mermaid! She asks you this riddle:  Alive without breath, As cold as death; Never thirsty, ever drinking, All in mail never clinking. What am I?",
+        riddleSuccess: "You are brilliant! You solved the mermaid's riddle, so she gives you her gold and allows you to return home.",
+        riddleFail: "What a stupid answer. The mermaid has tricked you and you will spend the rest of your days as her servant. It will not be fun."
+    };
+    //adds a random number of points between 1 and 5 to the score.
     function gainPoints() {
-        var max = 5;
-        var min = 1;
-        increaseScore = parseInt((Math.random() * (max - min + 1)),
-            10) + min;
-        knight.score += increaseScore;
-        scoreText.text(knight.score);
-    }
+            var max = 5;
+            var min = 1;
+            increaseScore = parseInt((Math.random() * (max - min + 1)),
+                10) + min;
+            knight.score += increaseScore;
+            scoreText.text(knight.score);
+        }
 
+    //subtracts a random number of points between 1 and 5 from the score.
     function losePoints() {
             var max = 5;
             var min = 1;
@@ -28,43 +46,47 @@ $(document).ready(function() {
             scoreText.text(knight.score);
         }
 
-        //start button, gives the choice of weapons
+    function restartGame() {
+          $("#fail").addClass("hidden");
+          $("#gold").addClass("hidden");
+          $("#start").removeClass("hidden");
+          promptText.text(decisions.welcome);
+          knight.score=0;
+          scoreText.text(knight.score);
+          $("#answer").val("");
+        }
+
+    //start button, gives the choice of weapons
     $("#start").click(function() {
         $("#start").addClass("hidden");
-        promptText.text(
-            "Good morning! It's time for an adventure. Choose your weapon?"
-        );
+        promptText.text(decisions.begin);
         //display harpoon and sword pictures
         $("#sword").removeClass("hidden");
         $("#crossbow").removeClass("hidden");
     });
-    //harpoon and sword buttons, change weapon value, give choice of destinations
-    //sword button
+    //sword button, change weapon value, give choice of destinations
     $("#sword").click(function() {
         knight.weapon = "sword";
-        promptText.text("Where does your adventure take you today?");
+        promptText.text(decisions.setting);
         gainPoints();
         $("#forest").removeClass("hidden");
         $("#sword").addClass("hidden");
         $("#sea").removeClass("hidden");
         $("#crossbow").addClass("hidden");
     });
-    //crossbow button
+    //crossbow button, change weapon value, give choice of destinations
     $("#crossbow").click(function() {
         knight.weapon = "crossbow";
-        promptText.text("Where does your adventure take you today?");
+        promptText.text(decisions.setting);
         gainPoints();
         $("#forest").removeClass("hidden");
         $("#sword").addClass("hidden");
         $("#sea").removeClass("hidden");
         $("#crossbow").addClass("hidden");
     });
-    //forest button, changes setting value, gives choice of enter cave or throw rock
+    //forest button,  gives choice of enter cave or throw rock
     $("#forest").click(function() {
-        knight.setting = "forest";
-        promptText.text(
-            "You've just stumbled on a creepy, dark cave. Do you throw a rock in first, or just march right in?"
-        );
+        promptText.text(decisions.forest);
         gainPoints();
         $("#cave").removeClass("hidden");
         $("#forest").addClass("hidden");
@@ -77,15 +99,11 @@ $(document).ready(function() {
         $("#rock").addClass("hidden");
         if (knight.weapon === "sword") {
             $("#gold").removeClass("hidden");
-            promptText.text(
-                "You have awoken the dragon and he runs out of the cave right at you! Thank goodness you brought your sword. You have slain the dragon, stolen its treasure, and return to your castle as a hero. Huzzah!"
-            );
+            promptText.text(decisions.defeatDragon);
             gainPoints();
         } else {
             $("#fail").removeClass("hidden");
-            promptText.text(
-                "You have awoken the dragon and he runs out of the cave right at you! Your weapon is no match for this beast. You should have chosen your sword. The dragon destroys you in a single bite and your entire family forgets about you by Christmas."
-            );
+            promptText.text(decisions.dragonDeath);
             losePoints();
         }
     });
@@ -94,17 +112,12 @@ $(document).ready(function() {
         $("#cave").addClass("hidden");
         $("#rock").addClass("hidden");
         $("#fail").removeClass("hidden");
-        promptText.text(
-            "What kind of knight just walks into a cave without checking for dragons first? Of course there was a dragon in there. He eats you and your family chalks it up to natural selection."
-        );
+        promptText.text(decisions.cave);
         losePoints();
     });
-    //sea button, changes setting value, gives choice of boat or beach
+    //sea button,  gives choice of boat or beach
     $("#sea").click(function() {
-        knight.setting = "sea";
-        promptText.text(
-            "There is an empty boat! What do you do: Continue to walk up the beach, or get in the boat and paddle out to sea?"
-        );
+        promptText.text(decisions.boat);
         gainPoints();
         $("#boat").removeClass("hidden");
         $("#forest").addClass("hidden");
@@ -117,44 +130,44 @@ $(document).ready(function() {
         $("#beach").addClass("hidden");
         if (knight.weapon === "crossbow") {
             $("#gold").removeClass("hidden");
-            promptText.text(
-                "You have encountered a sea monster! Thank goodness you brought your crossbow. You have slayed the sea monster and return to your castle a hero."
-            );
+            promptText.text(decisions.defeatSeaMonster);
             gainPoints();
         } else {
             $("#fail").removeClass("hidden");
-            promptText.text(
-                "You have encountered a sea monster! Your weapon is no match for this beast. You should have chosen your crossbow. The sea monster destroys you in a single bite and your entire family forgets about you by Christmas."
-            );
+            promptText.text(decisions.seaMonsterDeath);
             losePoints();
         }
     });
-    //beach button, gives you the mermaid riddle and a text box
+    //beach button, gives you the mermaid riddle, a text box, and a submit button
     $("#beach").click(function() {
         $("#boat").addClass("hidden");
         $("#beach").addClass("hidden");
+        $("#mermaid").removeClass("hidden");
+        $("#mermaid").addClass("ownLine");
         $("#submit").removeClass("hidden");
         $("#answer").removeClass("hidden");
-        promptText.text(
-            "You have encountered a mermaid! She asks you this riddle:  Alive without breath, As cold as death; Never thirsty, ever drinking, All in mail never clinking. What am I?"
-        );
+        promptText.text(decisions.mermaid);
     });
     //riddle text box submit button, evaluates answer, tells you if you are slave or get gold, ends game
     $("#submit").click(function() {
-      $("#submit").addClass("hidden");
-      $("#answer").addClass("hidden");
+        $("#mermaid").addClass("hidden");
+        $("#mermaid").removeClass("ownLine");
+        $("#submit").addClass("hidden");
+        $("#answer").addClass("hidden");
         if ($("#answer").val().toLowerCase() === "fish") {
-          $("#gold").removeClass("hidden");
-            promptText.text(
-                "You are brilliant! You solved the mermaid's riddle, so she gives you her gold and allows you to return home."
-            );
+            $("#gold").removeClass("hidden");
+            promptText.text(decisions.riddleSuccess);
             gainPoints();
         } else {
             $("#fail").removeClass("hidden");
-            promptText.text(
-                "What a stupid answer. The mermaid has tricked you and you will spend the rest of your days as her servant. It will not be fun."
-            );
+            promptText.text(decisions.riddleFail);
             losePoints();
         }
+    });
+    $("#fail").click(function() {
+      restartGame();
+  });
+  $("#gold").click(function() {
+      restartGame();
     });
 });
